1bf1d4110437060a821aa373bc55e44846fac091
Austin Do <92080459+foodboi88@users.noreply.github.com>
2023-12-21 17:36:36 +0700

    get rid of code

--- src/api/comment/comment.api.ts
+++ src/api/comment/comment.api.ts
@@ -1,15 +1,8 @@
 /* eslint-disable new-parens */
-import HttpClient from "../http-client";
-import SYSTEM_CONSTANTS from "../../common/constants";
-
-import {
-    GetAllMemberReq,
-    GetAllMembersWithRoleReq,
-} from "../../common/define-type";
 import { Observable } from "rxjs/internal/Observable";
 import { catchError, map } from "rxjs/operators";
 import { API_URL } from "../../enum/api.enum";
-import { IReqGetLatestSketchs } from "../../common/sketch.interface";
+import HttpClient from "../http-client";
 
 export default class CommentsApi {
     static apiURL = API_URL;
